cd386f1184606f978a9dc111b921dc3624724c33
IT2 <it2@AWT.local>
2020-07-21 15:29:12 -0500

    server running

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require("express");
-const server = express();
+const express_graphql = require('express-graphql').graphqlHTTP
+const {buildSchema} = require('graphql')
 
+
+
+//GrapgQL Schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`)
+// root resolvers
+const root = { message: () => 'Hello World!'}
+const server = express();
+server.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+
+}))
 
 module.exports = server;
